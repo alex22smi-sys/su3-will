f113ee7f10cbc6bc7ee2ad35ccd1c13b2f5156c7
alex22smi-sys <alex22smi@gmail.com>
2026-08-31 22:40:33 +0500

    Add files via upload

---
+++ _init_.py
@@ -0,0 +1,4 @@
+   """SU3-WILL: SU(3) Gauge-Covariant Neural Networks"""
+   __version__ = "3.4"
+   from .core import GaugeCovariantLayer, WillModule
+   __all__ = ["GaugeCovariantLayer", "WillModule"]
