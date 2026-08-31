343f6fd27a06a1dd7999e4ad2572bfb1330cd98e
alex22smi-sys <alex22smi@gmail.com>
2026-08-31 22:26:59 +0500

    Delete su3_will/_init_.py

--- su3_will/_init_.py
+++
@@ -1,9 +0,0 @@
-"""
-SU3-WILL: SU(3) Gauge-Covariant Neural Networks
-Physics = Psychology. Will + Trust + Communication
-"""
-__version__ = "3.4"
-
-from .core import GaugeCovariantLayer, WillModule
-
-__all__ = ["GaugeCovariantLayer", "WillModule"]
