0d1752f67227075f973659d1af8999d4d5d55917
alex22smi-sys <alex22smi@gmail.com>
2026-09-01 11:06:43 +0500

    Refactor __init__.py for SU3 Resonance GNN

--- su3_will/__init__.py
+++ su3_will/__init__.py
@@ -1,4 +1,27 @@
-"""SU3-WILL: SU(3) Gauge-Covariant Neural Networks"""
-__version__ = "3.4"
-from .core import GaugeCovariantLayer, WillModule
-__all__ = ["GaugeCovariantLayer", "WillModule"]
+"""
+SU(3) Gauge-Covariant Resonance GNN v3.4
+Gluon = tactile | Real photon = words | Virtual photon = thinking
+"""
+
+from .core import (
+    SU3GaugeField,
+    SU3ResonanceLayer,
+    SU3GaugeBlock,
+    SU3ResonanceGNN,
+    ResonanceDiagnostics,
+    GaugeDiagnostics,
+    get_gell_mann_matrices,
+    make_undirected,
+)
+
+__version__ = "3.4.0"
+__all__ = [
+    "SU3GaugeField",
+    "SU3ResonanceLayer", 
+    "SU3GaugeBlock",
+    "SU3ResonanceGNN",
+    "ResonanceDiagnostics",
+    "GaugeDiagnostics",
+    "get_gell_mann_matrices",
+    "make_undirected",
+]
